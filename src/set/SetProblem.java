acb43a1e473a8305f8c987a698465179a52e5e69
jiyounSeo <joanne901106@gmail.com>
2019-07-08 11:06:50 +0900

    변수명변경

--- src/set/SetProblem.java
+++ src/set/SetProblem.java
@@ -4,23 +4,21 @@ import java.util.Arrays;
 
 public class SetProblem {
     // https://programmers.co.kr/learn/courses/30/lessons/12938
-    // https://programmers.co.kr/learn/courses/30/lessons/12938
-
     public int[] solution(int n, int s) {
         if (s < n) {
             return new int[]{-1};
         } else if (n == 1) {
             return new int[]{s};
         }
-        int a = s/n;
-        int c = s%n;
+        int average = s/n;
+        int remain = s%n;
 
         int[] answer = new int[n];
-        Arrays.fill(answer, a);
+        Arrays.fill(answer, average);
 
-        if (c != 0) {
-            for (int i=1; i<=n-(n-c); i++) {
-                answer[n-i] = a+1;
+        if (remain != 0) {
+            for (int i=1; i<=remain; i++) {
+                answer[n-i] = average+1;
             }
         }
         return answer;
